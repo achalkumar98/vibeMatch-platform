d818ce782a00608680131d02639eb60366b57232
achalkumar98 <hackerachal1620@gmail.com>
2026-08-20 11:34:50 +0530

    improve docs and name of project in package

--- backend-app/src/docs/swaggerDef.js
+++ backend-app/src/docs/swaggerDef.js
@@ -4,7 +4,7 @@ const config = require('../config/config');
 const swaggerDef = {
   openapi: '3.0.0',
   info: {
-    title: 'devTinder API documentation',
+    title: 'VibeMatch API documentation',
     version,
     license: {
       name: 'ISC',
